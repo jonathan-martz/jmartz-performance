205322f57390cb291bd835136497bb8ef1b2b599
jonathan <bitbucket@jmartz.de>
2019-11-26 21:43:33 +0100

    [JMS-17] run execute parallel with taskParallelExec

--- RoboFile.php
+++ RoboFile.php
@@ -103,11 +103,13 @@ class RoboFile extends Tasks
         }
 
         if (count($pages) > 0) {
+            $exec = $this->taskParallelExec();
             foreach ($pages as $page) {
                 foreach ($page['urls'] as $url) {
-                    $this->_exec('lighthouse --output json --chrome-flags="--headless" --output-path ' . $folder . '/lighthouse-' . $url['title'] . '.json ' . $url['url']);
+                    $exec->process('lighthouse --output json --chrome-flags="--headless" --output-path ' . $folder . '/lighthouse-' . $url['title'] . '.json ' . $url['url']);
                 }
             }
+            $exec->run();
         }
     }
 
